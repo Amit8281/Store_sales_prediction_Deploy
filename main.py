dd7e071e7380bb1d25e4a6bd979f82820bb7f0a4
Amit8281 <ak15072419@gmail.com>
2023-08-25 09:14:15 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import numpy as np
+import sys
+from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
 
 
 
 #Data Ingestion
 if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    try:
+        #Data Ingestion
+        obj = DataIngestion()
+        train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+        
+    except Exception as e:
+        raise CustomException(e,sys)
